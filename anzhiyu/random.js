30deee265bcc9226be5bbf59e0a2b794aee6ce5b
fenghe666666 <1813756821@qq.com>
2024-01-06 17:38:50 +0800

    Site updated: 2024-01-06 17:38:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/05/hello-world/"];function toRandomPost(){
+var posts=["2024/01/06/aaaaaaaaaa/","2024/01/05/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
