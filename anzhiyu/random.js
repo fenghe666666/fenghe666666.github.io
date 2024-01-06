a4efee57ab03a80f7bffacff9fde8dd56ed3fccd
fenghe666666 <1813756821@qq.com>
2024-01-06 19:02:06 +0800

    Site updated: 2024-01-06 19:02:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/05/hello-world/","2024/01/06/易飞文字识别 免费在线批量图片文字识别工具/","2024/01/06/aaaaaaaaaa/"];function toRandomPost(){
+var posts=["2024/01/06/aaaaaaaaaa/","2024/01/06/易飞文字识别 免费在线批量图片文字识别工具/","2024/01/05/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
