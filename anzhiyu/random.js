c9249648cfd598c2ce9971ce5f3f5ef11fc4a764
fenghe666666 <1813756821@qq.com>
2024-01-07 12:06:09 +0800

    Site updated: 2024-01-07 12:06:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/06/aaaaaaaaaa/","2024/01/07/aaaaaaacccccc/","2024/01/05/hello-world/","2024/01/06/易飞文字识别 免费在线批量图片文字识别工具/","2024/01/06/云萌 Windows系统一键激活工具/"];function toRandomPost(){
+var posts=["2024/01/07/aaaaaaacccccc/","2024/01/06/aaaaaaaaaa/","2024/01/05/hello-world/","2024/01/06/云萌 Windows系统一键激活工具/","2024/01/06/易飞文字识别 免费在线批量图片文字识别工具/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
